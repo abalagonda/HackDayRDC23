f525fab437bc8de588ea283d6194f823b959f601
slee692 <slee692@illinois.edu>
2023-06-14 15:46:41 -0500

    working out frontend

--- currently-web/src/pages/Home/index.js
+++ currently-web/src/pages/Home/index.js
@@ -1,8 +1,10 @@
+import Header from "../../components/Header";
 import Map from "../../components/Map";
 const Home = () => {
   return (
     <div>
-      <Map />{" "}
+      <Header />
+      <Map />
     </div>
   );
 };
