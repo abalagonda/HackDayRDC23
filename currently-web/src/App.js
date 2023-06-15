a77d6c9c232a929b07e9ab21054eac207f2c9905
Josh Belser <joshbelser@Joshs-MacBook-Pro-4.local>
2023-06-14 21:03:42 -0500

    finished sidebar

--- currently-web/src/App.js
+++ currently-web/src/App.js
@@ -32,17 +32,35 @@
 import React from 'react';
 import Sidebar from './components/Sidebar/Sidebar';
 import Button from './components/Button/Button';
+import Event from './components/Event/Event'
+
+const attendees = ["John Doe", "Jane Smith", "Mike Johnson", "Emily Davis", "lorenzo", "aniketh", "carlos", "ved", "steven"];
+
 const App = () => {
+
   return (
     <div className="app">
 
       {/* <Sidebar /> */}
-      <Button />
+      {/* const e1 = new Event("Lunch w the Founder", "come join us on the quad fool");
+      const e2 = new Event("Movie", "pull up cuh"); */}
+      <div classname="event">
 
+        <Event classname="dance" event_name={"Dance"} event_desc={"We boolin"} attendees={attendees}></Event>
+      </div>
+      {/* <div classname="event">
+        <Event classname="dance" event_name={"Coffee"} event_desc={"We boolin"} attendees={["jgew fejhwiofj"]}></Event>
+      </div> */}
+      {/* <Event name={"Ride"} description={"We boolin"}></Event> */}
+      {/* <e1 />
+      <e2 /> */}
+      {/* <Button /> */}
       {/* Add the rest of your app's content here */}
     </div>
   );
 };
 
 export default App;
+
+
 
--- currently-web/src/components/Sidebar/Sidebar.js
+++ currently-web/src/components/Sidebar/Sidebar.js
@@ -2,64 +2,57 @@ import React, { useState } from 'react';
 import './Sidebar.css';
 import { Navigation } from 'react-minimal-side-navigation';
 import 'react-minimal-side-navigation/lib/ReactMinimalSideNavigation.css';
-
+import Event from '../Event/Event';
 // const Sidebar = () => {
-  function Sidebar() {
+function Sidebar({sidebar_name, sidebar_desc, attendees}) {
+  const sn = sidebar_name
+  const sd = sidebar_desc
+  const attendeeList = attendees
+
   const [isOpen, setIsOpen] = useState(false);
   const [description, setDescription] = useState('');
   const [showDescription, setShowDescription] = useState(false);
 
-  const toggleSidebar = () => {
-    setIsOpen(!isOpen);
-  };
-
+  const attendeeSubNav = attendeeList.map((attendee, index) => ({
+    title: attendee,
+    itemId: `/people/${index}`,
+  }));
   return (
-    // <>
-    //   <button className="hamburger" onClick={toggleSidebar}>
-    //     ☰
-    //   </button>
+    <div className="sidebar-container">
+      <div className="sidebar">
+        <h1 className="event-title">{sn}</h1>
+        <Navigation
+          activeItemId="/management/members"
+          onSelect={({ itemId }) => {
+            // Handle item selection logic here
+          }}
+          items={[
+            {
+              title: 'Description',
+              itemId: '/description',
+              subNav: [
+                {
+                  title: sd,
+                  itemId: '/description/description',
+                }
+              ],
+            },
+            {
+              title: 'Attendees',
+              itemId: '/people',
+              subNav: attendeeSubNav
+              // subNav: [
+              //   {
+              //     title: attendees[0],
+              //     ItemId: '/people/peps'
+              //   }
+              // ]
+            },
+          ]}
+        />
+      </div>
+    </div>
+)}
 
-    //   {isOpen && (
-        <div className="sidebar-container">
-          <div className="sidebar">
-            <h1 className="event-title">Coffee</h1>
-            <Navigation
-              activeItemId="/management/members"
-              onSelect={({ itemId }) => {
-                // Handle item selection logic here
-              }}
-              items={[
-                {
-                  title: 'Description',
-                  itemId: '/description',
-                  subNav: [
-                    {
-                      title: 'this is a description',
-                      itemId: '/description/description',
-                    }
-                  ],
-                },
-                {
-                  title: 'Attendees',
-                  itemId: '/people',
-                  subNav: [
-                    {
-                      title: 'Name 1',
-                      itemId: '/people/names',
-                    },
-                    {
-                      title: 'Name 2',
-                      itemId: '/people/names',
-                    },
-                  ],
-                },
-              ]}
-            />
-          </div>
-        </div>
-      )}
-//     </>
-//   );
-// };
 
 export default Sidebar;
