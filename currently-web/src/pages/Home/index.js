da4958738e77467674c825f523c135267e965f7d
Cathy Boman <cathyboman@MacBook-Pro-394.local>
2023-06-14 21:48:06 -0500

    new changes

--- currently-web/src/pages/Home/index.js
+++ currently-web/src/pages/Home/index.js
@@ -1,14 +1,12 @@
 import Neo4jGraph from "../../components/Graph/Graph";
 import Header from "../../components/Header";
-import Map from "../../components/Map";
+//import Map from "../../components/Map";
 import ANYTHINGIWANT from "../../components/Map/index2";
 const Home = () => {
   return (
     <div>
       <Header />
       <ANYTHINGIWANT />
-
-      {/* <Map /> */}
     </div>
   );
 };
